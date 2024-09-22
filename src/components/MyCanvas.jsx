5889a032ddf73a8c5d9dd5bfba6b7c2afcfa9e9c
FAT YODA <shivangsingh654@gmail.com>
2024-09-22 16:58:18 +0530

    hello

--- src/components/MyCanvas.jsx
+++ src/components/MyCanvas.jsx
@@ -1,21 +1,25 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Html } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { buffer, bufferAttribute } from 'three/webgpu';
 
-const pointsArray = new Float32Array(500 * 3);
+import { OrbitControls, Html } from '@react-three/drei';
+
+import { useEffect, useRef, useState } from 'react';
+import gsap from 'gsap';
+// const config = { fov: 75, position: [0, 0, 500] };
+
+const pointsArray = new Float32Array(5000 * 3);
 const planetsArray = [
 	{
+		key: 'first',
 		index: 1,
 		name: 'Mercury',
-		distanceFromSun: 579000, // in 100 kilometers
+		distanceFromSun: 579, // in 100 kilometers
 		acceleration: 0.037, // in 100 m/s^2
 		mass: 3.3011e21, // in 100 kilograms
 		radius: 24.397, // in 100 kilometers
 	},
 	{
+		key: 'second',
 		index: 2,
 		name: 'Venus',
 		distanceFromSun: 1082,
@@ -24,6 +28,8 @@ const planetsArray = [
 		radius: 60.518,
 	},
 	{
+		key: 'third',
+
 		index: 3,
 		name: 'Earth',
 		distanceFromSun: 1496,
@@ -32,6 +38,8 @@ const planetsArray = [
 		radius: 63.71,
 	},
 	{
+		key: 'fourth',
+
 		index: 4,
 		name: 'Mars',
 		distanceFromSun: 2279,
@@ -40,6 +48,8 @@ const planetsArray = [
 		radius: 33.99,
 	},
 	{
+		key: 'fifth',
+
 		index: 5,
 		name: 'Jupiter',
 		distanceFromSun: 7785,
@@ -48,6 +58,8 @@ const planetsArray = [
 		radius: 714.92,
 	},
 	{
+		key: 'sixth',
+
 		index: 6,
 		name: 'Saturn',
 		distanceFromSun: 1434,
@@ -56,6 +68,8 @@ const planetsArray = [
 		radius: 602.68,
 	},
 	{
+		key: 'seventh',
+
 		index: 7,
 		name: 'Uranus',
 		distanceFromSun: 2871,
@@ -64,6 +78,8 @@ const planetsArray = [
 		radius: 255.59,
 	},
 	{
+		key: 'eighth',
+
 		index: 8,
 		name: 'Neptune',
 		distanceFromSun: 4495,
@@ -72,104 +88,167 @@ const planetsArray = [
 		radius: 247.64,
 	},
 ];
-
-const size = 500;
-
+// let isOnCanvas = false;
+const size = 5000;
 for (let index = 0; index < size; index += 3) {
-	pointsArray[index] = Math.random() * 50 - 25;
-	pointsArray[index + 1] = Math.random() * 50 - 25;
-	pointsArray[index + 2] = Math.random() * 50 - 25;
+	pointsArray[index] = Math.random() * 1000 - 500 + 5;
+	pointsArray[index + 1] = Math.random() * 1000 - 500 + 5;
+	pointsArray[index + 2] = Math.random() * 1000 - 500 + 5;
 }
-
-function HookedCanvas() {
-	// const { state, gl } = useState();
-	// const viewport = useThree((state) => state.viewport);
-}
-
 export default function MyCanvas() {
-	const canvasRef = useRef();
-	function animate() {
-		if (canvasRef.current) {
-			for (let index = 0; index < 500; index += 3) {
-				pointsArray[index + 2] += 0.01;
-				if (pointsArray[index + 2] > 20) {
-					pointsArray[index + 2] = -81;
-				}
-			}
-			canvasRef.current.needsUpdate = true;
-		}
+	let [camera, setCamera] = useState(null);
+	const [animate, setAnimate] = useState(true);
+	const [activePlanet, setActivePlanet] = useState(null);
+	const planetRefArray = new Array(8);
+	planetRefArray[0] = useRef();
+	planetRefArray[1] = useRef();
+	planetRefArray[2] = useRef();
 
-		requestAnimationFrame(animate);
+	planetRefArray[3] = useRef();
+	planetRefArray[4] = useRef();
+	planetRefArray[5] = useRef();
+
+	planetRefArray[6] = useRef();
+	planetRefArray[7] = useRef();
+	planetRefArray[8] = useRef();
+
+	const canvasRef = useRef();
+
+	let fact = Math.PI * 2;
+
+	function animateLoop() {
+		if (animate) {
+			planetRefArray.forEach((planet) => {
+				if (planet.current) {
+					const planetPosition = planet.current.position;
+							const f =
+						planet.current.factor *
+						planet.current.acceleration *
+						100;
+					// const x = planetPosition.x;
+					const x = Math.cos((fact += 0.00001)) * f;
+					const y = Math.sin((fact += 0.00001)) * f;
+
+					planetPosition.setX(x);
+
+					planetPosition.setY(y);
+					// const active = activePlanet?.uuid == planet.current.uuid;
+					/* if (active) {
+						console.log(active);
+						camera.position.setX([planetPosition.x]);
+						camera.position.setY([planetPosition.y]);
+						camera.position.setZ([
+							planetPosition.z + planet.current.radius / 100 + 5,
+						]);
+					} */
+				}
+			});
+			requestAnimationFrame(animateLoop);
+		}
 	}
 
-	animate();
+	animateLoop();
+
 	return (
-		<Canvas
-			performance={{ min: 0.3, max: 0.4 }}
-			camera={{ fov: 75, near: 0.1, far: 2000, position: [0, 0, 100] }}>
-			{/* <HookedCanvas /> */}
-			<OrbitControls />
+		<>
+			<Canvas
+				onClick={(e) => {
+					setActivePlanet(null);
 
-			{/* 	<mesh>
-				<boxGeometry args={[2, 2, 2]} scale={[2, 5, 2]} />
-				<meshBasicMaterial color={'red'} />
-			</mesh> */}
-			{/* 
-			<group>
+					// camera.position.set([0, 0, 200]);
+				}}
+				// ref={canvasRef}
+				performance={{ min: 0.3, max: 0.4 }}
+				camera={{
+					fov: 77,
+					near: 0.1,
+					far: 2000,
+					position: [0, 0, 200],
+				}}
+				onCreated={(state) => {
+					setCamera(state.camera);
+					camera?.position.set([0, 0, 200]);
+				}}>
+				{/* <PerspectiveCamera  ref={canvasRef} position={[0,150,500]} near={25}/> */}
+
+				<OrbitControls minZoom={0.4} />
+
+				<points>
+					<bufferGeometry>
+						<bufferAttribute
+							ref={canvasRef}
+							array={pointsArray}
+							itemSize={3}
+							count={size}
+							attach={'attributes-position'}
+							needsUpdate
+						/>
+					</bufferGeometry>
+
+					<pointsMaterial size={0.3} color={0xffffff} />
+				</points>
+				{/*THE SUN IS HERE */}
 				<mesh>
-					<boxGeometry args={[2, 2, 2]} scale={[2, 5, 2]} />
-					<meshBasicMaterial color={'gray'} />
+					<sphereGeometry args={[15, 50, 50]} />
+					<meshBasicMaterial color={'orange'} />
 				</mesh>
-				<Html position={[1, 1, 1]} pointerEvents="none">
-					<span className="text-2xl text-green-400 pointer-events-none">
-						hi this is some text
-					</span>
-				</Html>
-			</group> */}
-
-			<points>
-				<bufferGeometry>
-					<bufferAttribute
-						ref={canvasRef}
-						array={pointsArray}
-						itemSize={3}
-						count={size}
-						attach={'attributes-position'}
-						needsUpdate
-					/>
-				</bufferGeometry>
-				<pointsMaterial size={0.5} color={0xff00ff} />
-			</points>
-
-			<group>
-				{planetsArray.map((planet, index) => {
-					return (
-						<>
-							<group key={planet.name + index}>
+				<group>
+					{planetsArray.map((planet, index) => {
+						return (
+							<group key={planet.key}>
 								<mesh
+									onClick={(e) => {
+										const current = planetRefArray[index].current;
+
+										setActivePlanet(current);
+
+										console.log(activePlanet?.uuid);
+
+										if (camera) {
+											camera.position.setX([
+												current.position.x,
+											]);
+
+											camera.position.setY([
+												current.position.y ,
+											]);
+											camera.position.setZ([
+												current.position.z + 5,
+											]);
+										}
+									}}
+									acceleration={planet.acceleration}
+									radius={planet.radius}
+									factor={
+										(Math.random() * planet.radius) / 10 + 2
+									}
+									ref={planetRefArray[index]}
 									position={[
-										-10 * index,
-										5 * index,
-										planet.distanceFromSun / 100,
+										-10 * index + planet.radius / 100 + 15,
+										-5 * index + planet.radius / 100 + 15,
+										planet.distanceFromSun / 100 +
+											planet.radius / 100 +
+											15,
 									]}>
 									<sphereGeometry
 										args={[planet.radius / 100, 25, 25]}
 									/>
-									<meshBasicMaterial
-										color={'blue'}
-										wireframe
-									/>
+									<meshBasicMaterial color={'cyan'} />
 									<Html
-										distanceFactor={planet.radius / 10}
-										className="text-white rounded-3xl p-2 bg-transparent text-2xl select-none">
+										distanceFactor={
+											planet.radius < 200
+												? planet.radius / 5
+												: planet.radius / 20
+										}
+										className="text-black rounded-3xl p-2 bg-white text-2xl select-none">
 										{planet.name}
 									</Html>
 								</mesh>
 							</group>
-						</>
-					);
-				})}
-			</group>
-		</Canvas>
+						);
+					})}
+				</group>
+			</Canvas>
+		</>
 	);
 }
