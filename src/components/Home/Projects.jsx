e32e2f8e2f5dfa1f54f5775c3e6eb219d42cbed8
FAT YODA <shivangsingh654@gmail.com>
2024-11-05 19:47:36 +0530

    hello

---
+++ src/components/Home/Projects.jsx
@@ -0,0 +1,33 @@
+
+
+export default function Projects() {
+	return (
+		<div className="m-12 w-96 h-max border-8 backdrop-blur-md absolute z-20 top-36 right-16 rounded-3xl flex flex-wrap justify-between items-center ">
+			<a
+				href="https://fatyoda3.github.io/"
+				target="_blank"
+				className="p-4 m-2 w-11/12 bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-bold font-mono text-white text-2xl rounded-lg sm:text-sm px-5 py-2.5 text-center me-2 mb-2">
+				my first webpage
+			</a>
+			<a
+				href="https://todoo-1gl.pages.dev/"
+				target="_blank"
+				className="p-4 m-2 w-11/12 bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-bold font-mono text-white text-2xl rounded-lg sm:text-sm px-5 py-2.5 text-center me-2 mb-2">
+				todo with local-storage and history as well
+			</a>
+			<a
+				href="https://portfolio-6yb.pages.dev/"
+				target="_blank"
+				className="p-4 m-2 w-11/12 bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-bold font-mono text-white text-2xl rounded-lg sm:text-sm px-5 py-2.5 text-center me-2 mb-2">
+				my snow effect webpage
+			</a>
+
+			<a
+				href="https://landing-page-ready-player-one.vercel.app/"
+				target="_blank"
+				className="p-4 m-2 w-11/12 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-bold font-mono text-white text-2xl rounded-lg sm:text-sm px-5 py-2.5 text-center me-2 mb-2">
+				first attempt at react webpage
+			</a>
+		</div>
+	);
+}
